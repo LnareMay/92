4cf8ff329a0e9bc5cc7b49801fb8e447db4a0e54
Gideok <86092907+LnareMay@users.noreply.github.com>
2024-12-11 11:20:59 +0900

    기초 프로젝트 구성
    기초 프로젝트 구성

--- src/test/java/com/lec/packages/ApplicationTests.java
+++ src/test/java/com/lec/packages/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.lec.packages;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
