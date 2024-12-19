93beb194ed6e3a0573f154294b1c47fd56d3dc15
hyejigpwl <happyyo3977@naver.com>
2024-12-19 11:18:09 +0900

    [혜지-회원가입] 퍼블 대략 완성

--- src/main/java/com/lec/packages/dto/MemberJoinDTO.java
+++ src/main/java/com/lec/packages/dto/MemberJoinDTO.java
@@ -13,15 +13,16 @@ public class MemberJoinDTO {
 	private String MEM_CLUB;
 	private String MEM_PICTURE;
 	private String MEM_INTRODUCTION;
-	private boolean MEM_GENDER;
+	private Boolean MEM_GENDER;
 	private String MEM_TELL;
 	private String MEM_EMAIL;
 	private String MEM_BIRTHDAY;
 	private String MEM_ADDRESS;
+	private String MEM_ADDRESS_DETAIL;
 	private String MEM_ZIPCODE;
 	private String MEM_ADDRESS_SET;
-	private boolean MEM_ISMANAGER;
-	private boolean DELETE_FLAG;
-	private boolean MEM_SOCIAL;
+	private Boolean MEM_ISMANAGER;
+	private Boolean DELETE_FLAG;
+	private Boolean MEM_SOCIAL;
 
 }
