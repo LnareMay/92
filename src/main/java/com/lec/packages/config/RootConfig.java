1129a6edf6a161f8ab3f047e316b05648531d8c8
hyejigpwl <happyyo3977@naver.com>
2024-12-19 20:48:24 +0900

    [혜지-헤더] 헤더 토글 버튼 url 주소 따라서 css 활성화되게 , notosans-kr 글씨체 적용

--- src/main/java/com/lec/packages/config/RootConfig.java
+++ src/main/java/com/lec/packages/config/RootConfig.java
@@ -2,19 +2,22 @@ package com.lec.packages.config;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
-public class RootConfig implements WebMvcConfigurer{
-	
-	  @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry.addResourceHandler("/**")
-	                .addResourceLocations("classpath:/templates/", "classpath:/static/")
-	                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
-	    }
+public class RootConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/", "classpath:/static/")
+				.setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+	}
 	
 }
--- src/main/java/com/lec/packages/controllers/AdminController.java
+++ src/main/java/com/lec/packages/controllers/AdminController.java
@@ -18,5 +18,39 @@ public class AdminController {
 		return "admin/admin_main"; 
 	    }
 	
+	 @GetMapping("/main")
+	 public String adminMainPage() {
+	     return "admin/Admin_Main"; 
+	 }
+	
+	 @GetMapping("/Admin_edit")
+	 public String editAdmin() {
+		 return "admin/Admin_edit";
+	 }
+
+	 @GetMapping("/Facility_add")
+	    public String addFaciltyPage() {
+		 return "admin/Facility_add";
+	 }
+	 
+	 @GetMapping("/Facility_detail")
+	 public String DetailFaciltyPage() {
+		 return "admin/Facility_detail";
+	 }
+	 
+	 @GetMapping("/Facility_edit")
+	 public String EditFaciltyPage() {
+		 return "admin/Facility_edit";
+	 }
+	 
+	 @GetMapping("/Facility_list")
+	 public String ListFaciltyPage() {
+		 return "admin/Facility_list";
+	 }
+	 
+	 @GetMapping("/calendar")
+	 public String Calendar() {
+		 return "admin/calendar";
+	 }
 	
 }
