e2f767fc8d3f1ad6ddc70e5552ad01ae461070c8
Gideok <86092907+LnareMay@users.noreply.github.com>
2024-12-23 17:10:57 +0900

    파일 업로드 진행 중
    파일 업로드 진행 중

--- src/main/java/com/lec/packages/controllers/ClubRestController.java
+++ src/main/java/com/lec/packages/controllers/ClubRestController.java
@@ -1,15 +1,98 @@
 package com.lec.packages.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import groovy.util.logging.Log4j2;
+import com.lec.packages.dto.UploadFileDTO;
+import com.lec.packages.dto.UploadResultDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Log4j2
 @RestController
 @RequestMapping("/club")
-@RequiredArgsConstructor
 public class ClubRestController {
     
+    @Value("${com.lec.upload.path}")
+    private String uploadPath;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<UploadResultDTO> uploadFile(@RequestBody UploadFileDTO uploadFileDTO){
+        log.info("do upLoadFileController");
+        
+        if(uploadFileDTO.getFiles() != null){
+            List<UploadResultDTO> list;
+
+            list = new ArrayList<>();
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                String originalFileName = multipartFile.getOriginalFilename();
+				log.info(originalFileName);
+				String uuid = UUID.randomUUID().toString();
+				log.info(uuid);
+				
+				Path savePath = Paths.get(uploadPath, uuid + "_" + originalFileName);
+				boolean isImage = false;
+				
+				try {
+					multipartFile.transferTo(savePath); // 실제로 저장할 파일 위치
+					
+					// 이미지파일이라면
+					if(Files.probeContentType(savePath).startsWith("image")) {
+						isImage = true;
+						File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalFileName);
+						Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+					}
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}  
+				
+				list.add(UploadResultDTO.builder()
+						.uuid(uuid)
+						.fileName(originalFileName)
+						.img(isImage)
+						.build());
+            });
+            return list;
+        }
+        return null;
+    }
+    
+	@GetMapping(value = "/view/{fileName}")
+	public ResponseEntity<Resource> viewImgFile(@RequestBody @PathVariable("fileName") String fileName) {
+
+		Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+		} catch (IOException e) {
+			return ResponseEntity.internalServerError().build();
+		}
+		
+		return ResponseEntity.ok().headers(headers).body(resource);
+	}
 }
