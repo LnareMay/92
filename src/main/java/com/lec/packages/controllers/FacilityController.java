ea06964ae6fb6b90a55ea0d6074873a69456d8cf
sSeHyun <49834411+sSeHyun@users.noreply.github.com>
2024-12-30 18:20:16 +0900

    user_facility_list
    user_facility_list

--- src/main/java/com/lec/packages/controllers/FacilityController.java
+++ src/main/java/com/lec/packages/controllers/FacilityController.java
@@ -1,9 +1,20 @@
 package com.lec.packages.controllers;
 
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+import com.lec.packages.dto.FacilityDTO;
+import com.lec.packages.dto.PageRequestDTO;
+import com.lec.packages.dto.PageResponseDTO;
+import com.lec.packages.service.FacilityService;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -13,6 +24,26 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class FacilityController {
 	
-	
+    private final FacilityService facilityService;
+	 
+	    @GetMapping("/main")
+	    public String ListFaciltyPage(HttpServletRequest request,PageRequestDTO pageRequestDTO , Model model) {
+	    	 
+	    	 String requestURI = request.getRequestURI();  	 
+ 
+			 PageResponseDTO<FacilityDTO> responseDTO = facilityService.list(pageRequestDTO);
+			
+			 log.info("............................."+responseDTO);
+			 
+			
+			 model.addAttribute("currentURI", requestURI); // requestURI를 모델에 추가
+			 model.addAttribute("facilities",responseDTO.getDtoList());	
+			 model.addAttribute("totalPages", responseDTO.getTotal());  //총페이지
+			 model.addAttribute("pageNumber", pageRequestDTO.getPage()); // 현재 페이지 번호
+			 model.addAttribute("pageSize", pageRequestDTO.getSize()); // 한 페이지당 항목 수
+			 
+			 return "facility/facility_main";		 
+		 }
+
 }
 	
--- src/main/java/com/lec/packages/repository/search/FacilitySearch.java
+++ src/main/java/com/lec/packages/repository/search/FacilitySearch.java
@@ -11,8 +11,7 @@ import com.lec.packages.domain.Facility;
 public interface FacilitySearch {
 	
 	Page<Facility> searchByUser(String userId, Pageable pageable);
-	Page<Facility> searchAll(String[] types,String keyword,Pageable pageable);
-	Page<Facility> searchAllImpl(String[] types,String keyword,Pageable pageable);
+	Page<Facility> searchAllImpl(String[] types,String[] keywords,Pageable pageable);
 
 	
 }
