a6f5024907b3a8096445eb9b00e116a83e79ffa0
Gideok <86092907+LnareMay@users.noreply.github.com>
2025-01-20 17:34:23 +0900

    [기덕] 클럽 단위 시설 예약 및 일정 목록
    [기덕] 클럽 단위 시설 예약 및 일정 목록

---
+++ src/main/java/com/lec/packages/dto/ClubReservationDTO.java
@@ -0,0 +1,24 @@
+package com.lec.packages.dto;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClubReservationDTO {
+    String reservationCode;
+    String facilityName;
+    String clubCode;
+    Integer count;
+    Integer nowMemCount;
+    LocalTime reservationStartTime;
+    Date reservationDate;
+    String ReservationProgress;
+}
--- src/main/java/com/lec/packages/repository/ReservationRepository.java
+++ src/main/java/com/lec/packages/repository/ReservationRepository.java
@@ -3,12 +3,20 @@ package com.lec.packages.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lec.packages.domain.Member;
 import com.lec.packages.domain.Reservation;
+import com.lec.packages.dto.ClubReservationDTO;
+import com.lec.packages.dto.ClubReservationInterface;
 
 public interface ReservationRepository extends JpaRepository<Reservation, String>{
 	 List<Reservation> findByMemId(String memId);
 
 	List<Reservation> findByFacilityCode(String facilityCode);
+
+	// 시설 예약 내역 클럽원 현재 인원 수
+    @Query(value = "select r.*, count(rml.MEM_ID) as nowMemCount from reservation r join reservation_member_list rml on r.club_code = rml.CLUB_CODE where r.club_code =:clubCode and r.RESERVATION_DATE > Now() group by r.RESERVATION_CODE", nativeQuery = true)
+    List<ClubReservationInterface> getClubResList(@Param("clubCode") String clubCode);
 }
